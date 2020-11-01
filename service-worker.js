bdb3c560700478fc0f622f8fb6aef31a6f98b33a
Kadaj <64706671+lastkdj@users.noreply.github.com>
2020-11-01 01:13:53 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/servicedesk/precache-manifest.6e48161e63c75f6db56557e8e14f2d39.js"
+  "/servicedesk/precache-manifest.56fe4d23b909f158e60b8191df6ede81.js"
 );
 
 self.addEventListener('message', (event) => {
